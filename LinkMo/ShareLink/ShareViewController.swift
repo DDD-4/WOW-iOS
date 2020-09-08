01504836398895403c5b7a52bd331115dbfd874b
bomin <kbm97121@swu.ac.kr>
2020-09-08 15:32:29 +0900

    Add share extension

---
+++ LinkMo/ShareLink/ShareViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ShareViewController.swift
+//  ShareLink
+//
+//  Created by 김삼복 on 2020/09/08.
+//  Copyright © 2020 김보민. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
