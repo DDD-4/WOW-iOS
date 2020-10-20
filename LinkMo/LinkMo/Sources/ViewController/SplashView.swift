bfbefa73904b0c6d545cdf452c38600ddc7c508c
taeuk <nous178@naver.com>
2020-10-21 08:58:06 +0900

    [splash] SwiftUI 화면적용

--- LinkMo/LinkMo/Sources/ViewController/SplashView.swift
+++ LinkMo/LinkMo/Sources/ViewController/SplashView.swift
@@ -13,19 +13,46 @@ struct SplashView: View {
     @State var isActive: Bool = false
     
     var body: some View {
-        VStack{
-            if self.isActive{
-                UIhomeVC()
-                
-            }else{
-                Text("Awesome Splash Screen")
-                    .font(Font.largeTitle)
+        GeometryReader{ proxy in
+            
+            
+            ZStack{
+                if self.isActive{
+                    UIhomeVC()
+                }else{
+                    
+                    VStack{
+                        Spacer()
+                        
+                        Rectangle()
+                            .fill(Color.pureblue)
+                            .frame(width: 125, height: 30)
+                            .cornerRadius(15)
+                            .shadow(color: Color.white.opacity(0.15), radius: 5, x: -3, y: 6)
+                            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 3, y: -6)
+                            
+                            .rotationEffect(Angle(degrees: 135))
+                            .padding(.bottom, 50)
+                        
+                        Text("linkmo")
+                        .font(.custom("GmarketSansLight", size: 35))
+                        .foregroundColor(.fontColor)
+                        
+                        Spacer()
+                    }
+                    .padding(.bottom, 200)
+                    
+                }
+                Spacer()
+                    .layoutPriority(1)
             }
-        }
-        .onAppear{
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                withAnimation{
-                    self.isActive = true
+            .background(Color.pureblue)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation{
+                        self.isActive = true
+                    }
                 }
             }
         }
@@ -37,11 +64,8 @@ struct UIhomeVC: UIViewControllerRepresentable {
     
     func makeUIViewController(context: Context) -> UINavigationController {
         UIStoryboard(name: "Home", bundle: nil)
-        .instantiateViewController(identifier: "MainId")
-        
-        
+            .instantiateViewController(identifier: "MainId")
     }
-    
     func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
         
     }
@@ -51,4 +75,8 @@ struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
         SplashView()
     }
+}
+extension Color{
+    static let pureblue = Color(red: 0/255, green: 17/255, blue: 232/255)
+    static let fontColor = Color(red: 117/255, green: 181/255, blue: 255/255)
 }
