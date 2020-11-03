ec07506fc14c2918220dd01f8676c0133de12c5e
bomin <kbm97121@swu.ac.kr>
2020-11-03 20:25:22 +0900

    [share second] imgview 4개 수정 중

--- LinkMo/ShareLink/ShareTableViewController.swift
+++ LinkMo/ShareLink/ShareTableViewController.swift
@@ -27,46 +27,29 @@ class ShareTableViewController: UIViewController {
 	let categoryLabel = UILabel()
 	var sectionIndex: Int = 0
 	var tablesectionAll: TableSection? = nil
+	let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 89, height: 24))
+	let buttonSet = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+	let saveLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 28, height: 22))
+	let dashView = UIView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		tableView.delegate = self
-		tableView.dataSource = self
-		tableView.register(ShareSecondTableViewCell.self, forCellReuseIdentifier: "ShareSecondTableViewCell")
-		tableView.rowHeight = UITableView.automaticDimension
-		tableView.estimatedRowHeight = UITableView.automaticDimension
-		tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
-		view.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100)
 		
 		viewModel.readSections(categoryId: categoryIndex)
-		.subscribe(onNext: {[weak self] sections in
-			self?.sectionList = sections })
-		.disposed(by: disposeBag)
-		
-		navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 78)
-		navigationController?.navigationBar.setBackgroundImage(UIImage(), for:.default)
-		navigationController?.navigationBar.shadowImage = UIImage()
-		navigationController?.navigationBar.layoutIfNeeded()
-		navigationController?.navigationBar.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100)
-		navigationController?.navigationBar.topItem?.title = ""
-		navigationController?.navigationBar.tintColor = .black
-		self.title = "카테고리 선택"
-		
-		categoryLabel.myLabel()
-		categoryLabel.text = categoryAll!.title + " >"
-		categoryLabel.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(categoryLabel)
-		NSLayoutConstraint.activate([
-			categoryLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30),
-			categoryLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 36),
-			categoryLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -36),
-			categoryLabel.heightAnchor.constraint(equalToConstant: 30)
-		])
+			.subscribe(onNext: {[weak self] sections in
+				self?.sectionList = sections })
+			.disposed(by: disposeBag)
 		
 		setConstraint()
-		
-		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "다음", style: .plain, target: self, action: #selector(addTapped))
-		
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		navigationController?.isNavigationBarHidden = true
+		view.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		navigationController?.isNavigationBarHidden = false
 	}
 	
 	@objc func addTapped(){
@@ -78,21 +61,106 @@ class ShareTableViewController: UIViewController {
 		vc.tablesectionAll = self.tablesectionAll
 		self.navigationController?.pushViewController(vc, animated: false)
 	}
+	
 	private func setConstraint() {
 		self.view.addSubview(tableView)
+		self.view.addSubview(categoryLabel)
+		self.view.addSubview(titleLabel)
+		self.view.addSubview(buttonSet)
+		self.view.addSubview(saveLabel)
+		view.addSubview(dashView)
+		tableView.delegate = self
+		tableView.dataSource = self
+		tableView.register(ShareSecondTableViewCell.self, forCellReuseIdentifier: "ShareSecondTableViewCell")
+		tableView.rowHeight = UITableView.automaticDimension
+		tableView.estimatedRowHeight = UITableView.automaticDimension
+		tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
 		tableView.translatesAutoresizingMaskIntoConstraints = false
 		tableView.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100)
+		categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+		categoryLabel.myLabel()
+		categoryLabel.text = categoryAll!.title + " >"
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		buttonSet.translatesAutoresizingMaskIntoConstraints = false
+		saveLabel.translatesAutoresizingMaskIntoConstraints = false
+		dashView.translatesAutoresizingMaskIntoConstraints = false
+		dashView.addDashedBorder()
+		
+		titleLabel.text = "linkmo"
+		titleLabel.textAlignment = .center
+		titleLabel.textColor = UIColor(red: 89/255, green: 86/255, blue: 109/255, alpha: 100)
+		titleLabel.font = UIFont(name:"GmarketSansLight",size:18)
+		
+		
+		buttonSet.layer.cornerRadius = 5
+		buttonSet.setImage(UIImage(named: "chevronLeft"), for: .normal)
+		buttonSet.setImage(UIImage(named: "chevronLeft"), for: .selected)
+		buttonSet.contentVerticalAlignment = .fill
+		buttonSet.contentHorizontalAlignment = .fill
+		buttonSet.addTarget(self, action: #selector(barbutton(_:)), for: .touchUpInside)
+		buttonSet.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100)
+		
+		saveLabel.text = "저장"
+		saveLabel.textAlignment = .center
+		saveLabel.textColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 100)
+		saveLabel.font = UIFont(name:"AppleSDGothicNeo-Regular",size:16)
+		
+		
 		NSLayoutConstraint.activate([
-			tableView.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 24),
+			tableView.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 40),
 			tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
 			tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-			tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+			tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+			
+			titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+			titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 24),
+			
+			buttonSet.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 14),
+			buttonSet.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+			
+			saveLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -20),
+			saveLabel.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+			
+			categoryLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 62),
+			categoryLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
+			categoryLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
+			categoryLabel.heightAnchor.constraint(equalToConstant: 30),
+			
+			dashView.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 7.5),
+			dashView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
+			dashView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24)
+			
 		])
+	}
+	
+	@objc func barbutton(_ sender: Any){
+		self.navigationController?.popViewController(animated: true)
 	}
 	
 	
 }
 class ShareSecondTableViewCell: UITableViewCell{
+	let imgView1: UIImageView = {
+		var img = UIImageView()
+		img = UIImageView(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
+		return img
+	}()
+	let imgView2: UIImageView = {
+		var img = UIImageView()
+		img = UIImageView(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
+		return img
+	}()
+	let imgView3: UIImageView = {
+		var img = UIImageView()
+		img = UIImageView(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
+		return img
+	}()
+	let imgView4: UIImageView = {
+		var img = UIImageView()
+		img = UIImageView(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
+		return img
+	}()
+	
 	let tableLabel: UILabel = {
 		let label = UILabel()
 		label.textColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 100)
@@ -100,7 +168,8 @@ class ShareSecondTableViewCell: UITableViewCell{
 
 		return label
 	}()
-	let imgView: UIImageView = {
+	
+	let checkView: UIImageView = {
 		var img = UIImageView()
 		img = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
 		img.image = UIImage(named: "checkmark")
@@ -109,14 +178,29 @@ class ShareSecondTableViewCell: UITableViewCell{
 	
 	private func setConstraint() {
 		contentView.addSubview(tableLabel)
-		contentView.addSubview(imgView)
+		contentView.addSubview(checkView)
+		contentView.addSubview(imgView1)
+		contentView.addSubview(imgView2)
+		contentView.addSubview(imgView3)
+		contentView.addSubview(imgView4)
 		tableLabel.translatesAutoresizingMaskIntoConstraints = false
-		imgView.translatesAutoresizingMaskIntoConstraints = false
+		checkView.translatesAutoresizingMaskIntoConstraints = false
+		imgView1.translatesAutoresizingMaskIntoConstraints = false
+		imgView2.translatesAutoresizingMaskIntoConstraints = false
+		imgView3.translatesAutoresizingMaskIntoConstraints = false
+		imgView4.translatesAutoresizingMaskIntoConstraints = false
 		NSLayoutConstraint.activate([
-			tableLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 36),
-			tableLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-			tableLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
-			tableLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+			imgView1.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+			
+			imgView2.leftAnchor.constraint(equalTo: imgView1.rightAnchor, constant: 2),
+			imgView2.centerYAnchor.constraint(equalTo: imgView1.centerYAnchor),
+			imgView3.topAnchor.constraint(equalTo: imgView1.bottomAnchor, constant: 2),
+			imgView3.centerXAnchor.constraint(equalTo: imgView1.centerXAnchor),
+			imgView4.leftAnchor.constraint(equalTo: imgView3.rightAnchor, constant: 2),
+			imgView4.centerYAnchor.constraint(equalTo: imgView3.centerYAnchor),
+			
+			tableLabel.leadingAnchor.constraint(equalTo: imgView2.leadingAnchor, constant: 8)
+			
 		])
 	}
 
@@ -153,19 +237,24 @@ extension ShareTableViewController: UITableViewDataSource{
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShareSecondTableViewCell", for: indexPath) as? ShareSecondTableViewCell else { return UITableViewCell() }
 		let section = sectionList[indexPath.row]
+		cell.imgView1.image = UIImage(named: "checkmark")
+		cell.imgView2.image = UIImage(named: "checkmark")
+		cell.imgView3.image = UIImage(named: "checkmark")
+		cell.imgView4.image = UIImage(named: "checkmark")
 		
-		cell.layer.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100).cgColor
-		cell.textLabel?.text = section.header
-		cell.textLabel?.textColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 100)
-		cell.textLabel?.font = UIFont(name:"AppleSDGothicNeo-Regular" , size: 16)
-		cell.textLabel?.translatesAutoresizingMaskIntoConstraints = false
-		cell.textLabel?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 36).isActive = true
-		cell.textLabel?.trailingAnchor.constraint(equalTo: tableView.layoutMarginsGuide.trailingAnchor).isActive = true
+//		cell.layer.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100).cgColor
+//		cell.textLabel?.text = section.header
+//		cell.textLabel?.textColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 100)
+//		cell.textLabel?.font = UIFont(name:"AppleSDGothicNeo-Regular" , size: 16)
+//		cell.textLabel?.translatesAutoresizingMaskIntoConstraints = false
+//		cell.textLabel?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 36).isActive = true
+//		cell.textLabel?.trailingAnchor.constraint(equalTo: tableView.layoutMarginsGuide.trailingAnchor).isActive = true
+		cell.tableLabel.text = section.header
 		
-		
-		cell.accessoryView = cell.imgView
+		cell.accessoryView = cell.checkView
 		cell.accessoryView?.isHidden = true
-		cell.accessoryView?.leadingAnchor.constraint(equalTo: cell.textLabel!.leadingAnchor).isActive = true
+//		cell.accessoryView?.leadingAnchor.constraint(equalTo: cell.textLabel!.leadingAnchor).isActive = true
+		cell.accessoryView?.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 300).isActive = true
 		cell.accessoryView?.topAnchor.constraint(equalTo: cell.textLabel!.topAnchor, constant: 20).isActive = true
 		cell.accessoryView?.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -34).isActive = true
 		
@@ -182,9 +271,21 @@ extension ShareTableViewController: UITableViewDataSource{
 
 extension UILabel {
 	func myLabel() {
-		textColor = UIColor(red: 136/255, green: 136/255, blue: 136/255, alpha: 100)
-		font = UIFont(name:"AppleSDGothicNeo-Bold",size:15)
+		textColor = UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 100)
+		font = UIFont(name:"AppleSDGothicNeo-Regular",size:16)
 		backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 251/255, alpha: 100)
 	}
 }
 
+extension UIView {
+    func addDashedBorder() {
+        let shapeLayer = CAShapeLayer()
+		shapeLayer.strokeColor = UIColor(red: 231/255, green: 234/255, blue: 240/255, alpha: 100).cgColor
+        shapeLayer.lineWidth = 1
+        let path = CGMutablePath()
+        path.addLines(between: [CGPoint(x: 0, y: self.frame.height),
+								CGPoint(x: UIScreen.main.bounds.width - 48, y: self.frame.height)])
+        shapeLayer.path = path
+        layer.addSublayer(shapeLayer)
+    }
+}
