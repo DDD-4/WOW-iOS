e952e03fec6ca3457c6cad4bc048df12ea81d4f1
taeuk <nous178@naver.com>
2020-11-27 15:05:52 +0900

    [LinkUpdate] textfield UI수정

--- LinkMo/LinkMo/Sources/ViewController/LinkTitleUpdateVC.swift
+++ LinkMo/LinkMo/Sources/ViewController/LinkTitleUpdateVC.swift
@@ -20,11 +20,11 @@ class LinkTitleUpdateVC: UIViewController, UITextFieldDelegate {
     
     // title
     let titleLbl = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
-    let titleTextfield = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    let titleTextfield = CustomField()
     
     // link
     let linkLbl = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
-    let linkTextfield = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    let linkTextfield = CustomField()
     
     // Button
     let confirmBtn = EMTNeumorphicButton(type: .custom)
@@ -164,7 +164,6 @@ class LinkTitleUpdateVC: UIViewController, UITextFieldDelegate {
         titleLbl.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)
         titleLbl.textColor = UIColor.appColor(.titleGray)
         
-        titleTextfield.backgroundColor = UIColor.white
         titleTextfield.keyboardType = .default
         titleTextfield.contentVerticalAlignment = .center
         titleTextfield.layer.cornerRadius = titleTextfield.frame.size.height / 2
@@ -194,7 +193,6 @@ class LinkTitleUpdateVC: UIViewController, UITextFieldDelegate {
         linkLbl.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)
         linkLbl.textColor = UIColor.appColor(.titleGray)
         
-        linkTextfield.backgroundColor = UIColor.white
         linkTextfield.keyboardType = .default
         linkTextfield.contentVerticalAlignment = .center
         linkTextfield.layer.cornerRadius = linkTextfield.frame.size.height / 2
@@ -258,5 +256,8 @@ class LinkTitleUpdateVC: UIViewController, UITextFieldDelegate {
     @objc func barbutton(_ sender: Any){
         self.navigationController?.popViewController(animated: true)
     }
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.titleTextfield.resignFirstResponder()
+        self.linkTextfield.resignFirstResponder()
+    }
 }
